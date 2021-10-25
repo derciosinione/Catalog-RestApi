838a56d6913978bae576d352c6e75e22052677cc
derciosinione <derciosinione@gmail.com>
2021-10-25 20:10:49 +0100

    Clean Up

---
+++ Catalog.Api/Controllers/ItemsController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Catalog.Api.Models;
+using Catalog.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemoryRepository _repository;
+
+        public ItemsController(InMemoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return _repository.GetItems();
+        }
+    }
+}
